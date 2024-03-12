20ed889c3e802cb26c5bac83df3339b57ddf424f
dreamik <dream-ik89@naver.com>
2024-03-12 18:57:41 +0900

    feat: change deprecated code in spring security

--- src/main/java/com/cheer/cheerchat/config/CheerChatConfig.java
+++ src/main/java/com/cheer/cheerchat/config/CheerChatConfig.java
@@ -1,11 +1,17 @@
 package com.cheer.cheerchat.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,36 +21,35 @@ import java.util.Arrays;
 @Configuration
 public class CheerChatConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                .authorizeHttpRequests(authorize->authorize.requestMatchers(("/api/**")).authenticated()
-                        .anyRequest().permitAll()
-                )
-                .addFilterBefore(null,null)
-                .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource())
-                .and().formLogin()
-                .and().httpBasic()
+    public SecurityFilterChain securityFilterChain(final @NotNull HttpSecurity http) throws Exception{
+        http.httpBasic(HttpBasicConfigurer::disable)
+            .csrf(CsrfConfigurer::disable)
+            .cors(Customizer.withDefaults())
+            .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize ->
+                authorize
+                    .requestMatchers("/api/**").permitAll()
+                    .requestMatchers("/api-docs/swagger-config","/sign-in", "sign-up").permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+//                      .requestMatchers("/api/chat/**").hasRole(ROLE-enum)
+//                    .anyRequest().authenticated()
+            )
+//            .exceptionHandling(exceptionConfig ->
+//                    exceptionConfig.authenticationEntryPoint(로그인 실패 엔드포인트 설정).accessDeniedHandler(입력 거절 처리)
+//            )
+            .formLogin((formLogin) ->
+                    formLogin
+                            .loginPage("/login/login")
+                            .usernameParameter("username")
+                            .passwordParameter("password")
+                            .loginProcessingUrl("/login/login-proc")
+                            .defaultSuccessUrl("/", true)
+            )
+            .logout((logoutConfig) ->
+                    logoutConfig.logoutSuccessUrl("/")
+            )
+//            .userDetailsService(회원정보 관리 UserDetailsService)
         ;
-        return httpSecurity.build();
+        return http.build();
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.applyPermitDefaultValues();
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
 }
