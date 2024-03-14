edbecb50b70bfc5b4105d5307af83fcd01f0355c
dreamik <dream-ik89@naver.com>
2024-03-14 12:52:43 +0900

    feat: JWT validator(for filter)

--- src/main/java/com/cheer/cheerchat/config/CheerChatConfig.java
+++ src/main/java/com/cheer/cheerchat/config/CheerChatConfig.java
@@ -1,30 +1,29 @@
 package com.cheer.cheerchat.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class CheerChatConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(final @NotNull HttpSecurity http) throws Exception{
         http.httpBasic(HttpBasicConfigurer::disable)
-            .csrf(CsrfConfigurer::disable)
-            .cors(Customizer.withDefaults())
             .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize ->
                 authorize
@@ -32,24 +31,46 @@ public class CheerChatConfig {
                     .requestMatchers("/api-docs/swagger-config","/sign-in", "sign-up").permitAll()
                     .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
 //                      .requestMatchers("/api/chat/**").hasRole(ROLE-enum)
-//                    .anyRequest().authenticated()
+                    .anyRequest().authenticated()
             )
+            .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+            .csrf(CsrfConfigurer::disable)
+            .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
 //            .exceptionHandling(exceptionConfig ->
 //                    exceptionConfig.authenticationEntryPoint(로그인 실패 엔드포인트 설정).accessDeniedHandler(입력 거절 처리)
 //            )
-            .formLogin((formLogin) ->
+            .formLogin(formLogin ->
                     formLogin
-                            .loginPage("/login/login")
-                            .usernameParameter("username")
-                            .passwordParameter("password")
-                            .loginProcessingUrl("/login/login-proc")
+//                            .loginPage("/login/login")
+//                            .usernameParameter("username")
+//                            .passwordParameter("password")
+//                            .loginProcessingUrl("/login/login-proc")
                             .defaultSuccessUrl("/", true)
             )
-            .logout((logoutConfig) ->
+            .logout(logoutConfig ->
                     logoutConfig.logoutSuccessUrl("/")
             )
 //            .userDetailsService(회원정보 관리 UserDetailsService)
         ;
         return http.build();
+    }
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOrigins(List.of(
+                "http://localhost:3000/" // react 포트
+        ));
+        cfg.setAllowedMethods(Collections.singletonList("*"));
+        cfg.setAllowCredentials(true);
+        cfg.setAllowedHeaders(Collections.singletonList("*"));
+        cfg.setExposedHeaders(List.of("Authorization"));
+        cfg.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",cfg);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/cheer/cheerchat/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.cheer.cheerchat.config;
+
+public class JwtConstant {
+    public static final String JWT_HEADER = "Autorization";
+    public static final String SECRET_KEY = "sdnfjsrbwqehuzyhhbwegrggvgviyughhhuguy,jiuyubgyfdrtdf";
+}
