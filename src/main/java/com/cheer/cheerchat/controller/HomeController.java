05502ea406af1f99f39783272e88fd91bddf141d
dreamik <dream-ik89@naver.com>
2024-03-18 15:32:23 +0900

    fix: change directory

--- src/main/java/com/cheer/cheerchat/controller/HomeController.java
+++ src/main/java/com/cheer/cheerchat/controller/HomeController.java
@@ -1,8 +1,9 @@
-package com.cheer.controller;
+package com.cheer.cheerchat.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public ResponseEntity<String> homeController(){
-        return new ResponseEntity<>("Welcome to cheer-api", HttpStatus.OK);
+        return new ResponseEntity<>("Welcome to cheer-api using spring boot", HttpStatus.OK);
     }
 }
