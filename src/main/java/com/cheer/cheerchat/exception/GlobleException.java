36f671e0a3eeb2c86882d82c0676c63ee8edf8e8
dreamik <dream-ik89@naver.com>
2024-03-19 13:33:29 +0900

    feat: add exceptionHandlers: 3
    - messageExceptionHandler
    - methodArgsNotValidException
    - handleNoHandlerFoundException

--- src/main/java/com/cheer/cheerchat/exception/GlobleException.java
+++ src/main/java/com/cheer/cheerchat/exception/GlobleException.java
@@ -2,27 +2,44 @@ package com.cheer.cheerchat.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobleException {
     @ExceptionHandler(UserException.class)
-    public ResponseEntity<ErrorDetail> userExceptionHandler(UserException e, WebRequest req){
-        ErrorDetail err = new ErrorDetail(
-                e.getMessage(),
-                req.getDescription(false),
-                LocalDateTime.now());
+    public ResponseEntity<ErrorDetail> userExceptionHandler(UserException ue, WebRequest req){
+        ErrorDetail err = new ErrorDetail(ue.getMessage(),req.getDescription(false),LocalDateTime.now());
         return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
     }
-    //TODO add exceptionHandler
+
+    @ExceptionHandler(MessageException.class)
+    public ResponseEntity<ErrorDetail> messageExceptionHandler(MessageException me, WebRequest req){
+        ErrorDetail err = new ErrorDetail(me.getMessage(),req.getDescription(false),LocalDateTime.now());
+        return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetail> methodArgumentNotValidException(MethodArgumentNotValidException ve){
+        String methodValidationError = Objects.requireNonNull(ve.getBindingResult().getFieldError()).getDefaultMessage();
+        ErrorDetail err = new ErrorDetail("Validation Error",methodValidationError,LocalDateTime.now());
+        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorDetail> handleNoHandlerFoundException(NoHandlerFoundException ne){
+        ErrorDetail error = new ErrorDetail("Endpoint not found",ne.getMessage(),LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetail> otherExceptionHandler(UserException e, WebRequest req){
+    public ResponseEntity<ErrorDetail> otherExceptionHandler(Exception e, WebRequest req){
         ErrorDetail err = new ErrorDetail(
                 e.getMessage(),
                 req.getDescription(false),
