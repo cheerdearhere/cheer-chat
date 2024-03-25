4fee301a48e7759b5e61319ae8453d49852d0931
dreamik <dream-ik89@naver.com>
2024-03-25 15:52:07 +0900

    feat: remove and delete for chat

--- src/main/java/com/cheer/cheerchat/service/ChatService.java
+++ src/main/java/com/cheer/cheerchat/service/ChatService.java
@@ -5,21 +5,19 @@ import com.cheer.cheerchat.entity.User;
 import com.cheer.cheerchat.exception.ChatException;
 import com.cheer.cheerchat.exception.UserException;
 import com.cheer.cheerchat.request.GroupChatRequest;
-import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
 public interface ChatService {
-    public Chat createChat(User reqUser, List<Integer> userIdList) throws UserException;
-    public Chat findChatById(Integer chatId) throws ChatException;
-    public List<Chat> findAllChatByUserId(Integer userId) throws UserException;
-    public Chat updateGroupImg(Integer chatId, User reqUser, GroupChatRequest req) throws ChatException, UserException;
-    public Chat addUserToGroup(Integer reqUserId, Integer userId, Integer chatId) throws UserException, ChatException;
-    public Chat renameGroup(Integer chatId, String groupName, Integer reqUserId) throws ChatException, UserException;
-    public Chat updateGroupHost(Integer chatId, Integer newHostId, Integer reqUserId) throws ChatException, UserException;
-    public Chat removeFromGroup(Integer chatId, Integer userId, Integer reqUserId) throws ChatException, UserException;
-    public Chat deleteChat(Integer chatId, Integer reqUserId) throws ChatException, UserException;
-
-
-
+    Chat createChat(User reqUser, List<Integer> userIdList) throws UserException;
+    Chat findChatById(Integer chatId) throws ChatException;
+    List<Chat> findAllChatByUserId(Integer userId) throws UserException;
+    Chat updateGroupImg(Integer chatId, User reqUser, GroupChatRequest req) throws ChatException, UserException;
+    Chat addUserToGroup(Integer reqUserId, Integer userId, Integer chatId) throws UserException, ChatException;
+    Chat renameGroup(Integer chatId, String groupName, Integer reqUserId) throws ChatException, UserException;
+    Chat updateGroupHost(Integer chatId, Integer newHostId, Integer reqUserId) throws ChatException, UserException;
+    Chat removeFromGroup(Integer chatId, Integer userId, Integer reqUserId) throws ChatException, UserException;
+    Chat deleteChat(Integer chatId, Integer reqUserId) throws ChatException, UserException;
+    void removeChat(Integer chatId, Integer hostId)throws ChatException, UserException;
 }
--- src/main/java/com/cheer/cheerchat/service/ChatServiceImp.java
+++ src/main/java/com/cheer/cheerchat/service/ChatServiceImp.java
@@ -143,22 +143,95 @@ public class ChatServiceImp implements ChatService{
     public Chat removeFromGroup(Integer chatId, Integer userId, Integer reqUserId) throws ChatException, UserException {
         Optional<User> reqUserOpt = userRepository.findById(reqUserId);
         if(reqUserOpt.isEmpty()) throw new UserException("User not found by user Id: "+reqUserId);
-        Optional<User> newHostOpt = userRepository.findById(userId);
-        if(newHostOpt.isEmpty()) throw new UserException("User not found by new host Id: "+userId);
+        Optional<User> targetUserOpt = userRepository.findById(userId);
+        if(targetUserOpt.isEmpty()) throw new UserException("User not found by target user Id: "+userId);
         Optional<Chat> chatOpt = chatRepository.findById(chatId);
         if(chatOpt.isEmpty()) throw new ChatException("Chat not found by chat Id: "+chatId);
 
+        Chat chat = chatOpt.get();
+        Set<User> userSet = chat.getChatUsers();
+        User requireUser = reqUserOpt.get();
+        User targetUser = targetUserOpt.get();
+        //소속 확인
+        if(!userSet.contains(requireUser) || !userSet.contains(targetUser))
+            throw new UserException(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        // 요청자가 host
+        if(Objects.equals(chat.getChatHost(),requireUser)){
+            //host가 나가면 방기능 off
+            userSet.remove(requireUser);
+            chat.setChatUsers(userSet);
+            chatRepository.save(chat);
+            return this.deleteChat(chatId, reqUserId);
+        }
+        else if(Objects.equals(requireUser,targetUser)){// 본인이 채팅방을 나가는 경우
+            userSet.remove(targetUser);
+            chat.setChatUsers(userSet);
 
-
-        return null;
+            int chatUserNum = chat.getChatUsers().size();
+            if(chatUserNum < 1) // 0 이하는 잘못된 접근
+                throw new ChatException(HttpStatus.BAD_REQUEST.getReasonPhrase());
+            else if(chatUserNum == 1) // 혼자 남은 방
+                return this.deleteChat(chatId,reqUserId);
+            else if( chatUserNum == 2)  {// 2명만 남은방 == no group
+                chat.setGroup(false);
+                updateIdAndDate(reqUserId,chat);
+                return chatRepository.save(chat);
+            }
+            else { // 2명 이상 남은 방
+                updateIdAndDate(reqUserId,chat);
+                return chatRepository.save(chat);
+            }
+        }
+        else // 호스트가 아닌 채팅 유저가 타인을 제거하는 경우
+            throw new ChatException("no Auth : You Can't remove another user");
     }
 
+    /**
+     * just set unused chat, not remove data
+     * @param chatId target chat sequence
+     * @param reqUserId requiring user sequence
+     * @return chat
+     */
     @Override
     public Chat deleteChat(Integer chatId, Integer reqUserId) throws ChatException, UserException {
-        return null;
+        Optional<User> reqUserOpt = userRepository.findById(reqUserId);
+        if(reqUserOpt.isEmpty()) throw new UserException("User not found by user Id: "+reqUserId);
+        Optional<Chat> chatOpt = chatRepository.findById(chatId);
+        if(chatOpt.isEmpty()) throw new ChatException("Chat not found by chat Id: "+chatId);
+
+        Chat chat = chatOpt.get();
+        if(!Objects.equals(chat.getChatHost(),reqUserOpt.get()))
+            throw new ChatException("This user is not host in this chat: "+reqUserId);
+        else{
+            chat.setUseYn('0');
+            chat.setModId(reqUserId);
+            chat.setModDate(LocalDateTime.now());
+            //update
+            return chatRepository.save(chat);
+        }
     }
 
-    private void updateIdAndDate(Integer modifierId, Chat chat){
+    /**
+     * complete remove on database
+     * @param chatId target chat sequence number
+     * @param hostId requiring user sequence number
+     */
+    @Override
+    public void removeChat(Integer chatId, Integer hostId)throws ChatException, UserException {
+        Optional<User> reqUserOpt = userRepository.findById(hostId);
+        if (reqUserOpt.isEmpty()) throw new UserException("User not found by user Id: " + hostId);
+        Optional<Chat> chatOpt = chatRepository.findById(chatId);
+        if (chatOpt.isEmpty()) throw new ChatException("Chat not found by chat Id: " + chatId);
+
+        Chat chat = chatOpt.get();
+        if(!Objects.equals(chat.getChatHost(),reqUserOpt.get()))
+            throw new ChatException("This user is not host in this chat: "+hostId);
+        else{
+            chatRepository.deleteById(chatId);
+        }
+    }
+// private methods
+        private void updateIdAndDate(Integer modifierId, Chat chat){
         chat.setModId(modifierId);
         chat.setModDate(LocalDateTime.now());
     }
