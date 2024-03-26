63341bac164290cc753e7ad58cba23a6e3022c21
dreamik <dream-ik89@naver.com>
2024-03-26 23:33:03 +0900

    chore: use Getter, Setter with lombok 2

--- src/main/java/com/cheer/cheerchat/response/ApiResponse.java
+++ src/main/java/com/cheer/cheerchat/response/ApiResponse.java
@@ -1,9 +1,11 @@
 package com.cheer.cheerchat.response;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter @Setter
 public class ApiResponse {
 
     private String message;
