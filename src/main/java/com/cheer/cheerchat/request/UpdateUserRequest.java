3f9d68ca75883031873e7c2c5654bc5ba3f59abf
dreamik <dream-ik89@naver.com>
2024-03-26 23:32:38 +0900

    chore: use Getter, Setter with lombok

--- src/main/java/com/cheer/cheerchat/request/UpdateUserRequest.java
+++ src/main/java/com/cheer/cheerchat/request/UpdateUserRequest.java
@@ -1,25 +1,11 @@
 package com.cheer.cheerchat.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class UpdateUserRequest {
     private String fullName;
     private String profilePicture;
-
-    public UpdateUserRequest(){
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
+    
 }
