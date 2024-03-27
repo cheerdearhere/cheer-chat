792e4049521f2b4735299b64563db07c9b38dbcd
dreamik <dream-ik89@naver.com>
2024-03-27 14:01:07 +0900

    feat: add message's fields

--- src/main/java/com/cheer/cheerchat/entity/Message.java
+++ src/main/java/com/cheer/cheerchat/entity/Message.java
@@ -1,10 +1,7 @@
 package com.cheer.cheerchat.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -16,19 +13,24 @@ public class Message {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="MESSAGE_ID")
     private Integer id;
+    private String title;
+
     private String content;
 
     private Integer regId;
     private Integer modId;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
+    private char useYn;
 
+    @Enumerated(EnumType.ORDINAL)
+    private MessageContentType messageContentType;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="USER_ID")
     private User user;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="CHAT_ID")
     private Chat chat;
-    private char useYn;
+
 
 }
