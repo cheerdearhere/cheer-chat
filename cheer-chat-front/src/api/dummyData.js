f30afa27429125a79ba1d54fd56c9e990d5bfd3f
dreamik <dream-ik89@naver.com>
2024-04-06 19:16:16 +0900

    data: use dummy data

---
+++ cheer-chat-front/src/api/dummyData.js
@@ -0,0 +1,44 @@
+
+const dummyUserProfile = {
+    name:"username",
+    id: 0,
+    profile:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+}
+const dummyChatData=[
+    {
+        id: 4,
+        name: "user01",
+        profile: "https://cdn.pixabay.com/photo/2016/11/18/12/14/owl-1834152_1280.jpg",
+        message: "this is test message",
+        timestamp: "2022-01-01",
+        notice: 5,
+    },
+    {
+        id: 2,
+        name: "user02",
+        profile: "https://cdn.pixabay.com/photo/2016/11/18/12/14/owl-1834152_1280.jpg",
+        message: "this is test message",
+        timestamp: "2021-12-01",
+        notice: 4,
+    },
+    {
+        id: 1,
+        name: "user03",
+        profile: "https://cdn.pixabay.com/photo/2016/11/18/12/14/owl-1834152_1280.jpg",
+        message: "this is test message",
+        timestamp: "2021-11-01",
+        notice: 3,
+    },
+    {
+        id: 0,
+        name: "user04",
+        profile: "https://cdn.pixabay.com/photo/2016/11/18/12/14/owl-1834152_1280.jpg",
+        message: "this is test message",
+        timestamp: "2021-10-01",
+        notice: 2,
+    }
+]
+export {
+    dummyUserProfile,
+    dummyChatData,
+}
