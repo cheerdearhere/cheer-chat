f2ce371a2cde4bd03d33867d6ec8a6f7624565ae
dreamik <dream-ik89@naver.com>
2024-04-24 17:15:14 +0900

    feat: fix event callback function's name and use default data

--- cheer-chat-front/src/api/dummyData.js
+++ cheer-chat-front/src/api/dummyData.js
@@ -1,10 +1,11 @@
+//TODO 서버와 프론트의 data 일치시키기
 
 const dummyUserProfile = {
     name:"username",
     id: 0,
     profile:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
 }
-const dummyChatData=[
+const dummyChatListData=[
     {
         id: 4,
         name: "user01",
@@ -38,12 +39,45 @@ const dummyChatData=[
         notice: 2,
     }
 ];
+const defaultChatData={
+    chatId : 1,
+    hostId: 0,
+    userList: [
+        {
+            name:"username",
+            id: 0,
+            profile:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+        },
+        {
+            name: "user03",
+            id: 1,
+            profile: "https://cdn.pixabay.com/photo/2016/11/18/12/14/owl-1834152_1280.jpg"
+        }
+    ],
+    messages : [
+        {
+            messageId: 1,
+            senderId: 1,
+            messageType: 1,
+            message: "guest test msg",
+            img: ""
+        },
+        {
+            messageId: 0,
+            senderId: 0,
+            messageType: 1,
+            message: "host test msg",
+            img: ""
+        }
+    ]
+}
 const logoImg="https://cdn.pixabay.com/animation/2023/12/22/00/19/00-19-58-407_512.gif"
 const defaultInfo= "Send and receive message without keeping your phone online.\n"
     + "Use ChatApp on Up to 4 Linked devices and 1 phone at the same time."
 export {
     logoImg,
     defaultInfo,
+    defaultChatData,
     dummyUserProfile,
-    dummyChatData,
+    dummyChatListData,
 }
